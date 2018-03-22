55d329ffc4b047083f877346905afb464f3760c2
Jack <jsim89@gmail.com>
2018-03-23 05:36:33 +0800

    feat(BaseLibrary): Added BaseForm parent form for forms called by other forms

---
+++ BaseLibrary/BaseForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace BaseLibrary
+{
+    partial class BaseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Back_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Back_Button
+            // 
+            this.Back_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Back_Button.Location = new System.Drawing.Point(120, 160);
+            this.Back_Button.Name = "Back_Button";
+            this.Back_Button.Size = new System.Drawing.Size(75, 29);
+            this.Back_Button.TabIndex = 0;
+            this.Back_Button.Text = "Back";
+            this.Back_Button.UseVisualStyleBackColor = true;
+            // 
+            // BaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Back_Button);
+            this.Name = "BaseForm";
+            this.Text = "BaseForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        protected System.Windows.Forms.Button Back_Button;
+    }
+}
